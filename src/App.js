eb801c33c7e1ce7625ab90dc64483efc8f3c326b
phamngocthai <ngocthaipham99@gmail.com>
2021-09-30 06:08:07 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ function App() {
                     </div>
                   </div>
 
-                  <div data-aos="fade-up" className="section">
+                  <div
+                    //  data-aos="fade-up"
+                    className="section"
+                  >
                     <Content
                       tag={"ABOUT"}
                       description="My approach to building"
@@ -46,24 +49,61 @@ function App() {
                       }
                     />
                   </div>
-                  <div data-aos="fade-right" className="section">
+                  <div
+                    // data-aos="fade-right"
+                    className="section"
+                  >
                     <Content
                       tag={"SERVICE"}
-                      description="What I can do ?"
+                      description="What I can do"
                       content={
                         <p>
-                          "Lorem Ipsum is simply dummy text of the printing and
-                          typesetting industry. Lorem Ipsum has been the
-                          industry's standard dummy text ever since the 1500s,
-                          when an unknown printer took a galley of type and
-                          scrambled it to make a type specimen book. It has
-                          survived not only five centuries, but also the leap
-                          into electronic typesetting."
+                          Lorem ipsum dolor sit amet, consectetur. adipiscing
+                          elit. Aenean et elementum purus.
                         </p>
                       }
                     />
                   </div>
-                 
+                  <div
+                    // data-aos="fade-right"
+                    className="section"
+                  >
+                    <Content
+                      tag={"RESUME"}
+                      description="Experience"
+                      content={
+                        <div>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur. adipiscing
+                            elit. Aenean et elementum purus.
+                          </p>
+                          <div className="resume">
+                            <div className="resume-item">
+                              <p className="small">
+                                Internship :<span style={{marginLeft: "10px"}}>2021-Now</span>
+                              </p>
+                              <p style={{color: "white"}}>Intership</p>
+                              <p>Lorem Ipsum is simply dummy text of the printing industry.</p>
+                            </div>
+                            <div className="resume-item">
+                            <p className="small">
+                                Self study :<span style={{marginLeft: "10px"}}>2020-2021</span>
+                              </p>
+                              {/* <p style={{color: "white"}}>Student</p> */}
+                              <p>Lorem Ipsum is simply dummy text of the printing industry.</p>
+                            </div>
+                            <div className="resume-item">
+                            <p className="small">
+                                Nong Lam University :<span style={{marginLeft: "10px"}}>2017-2020</span>
+                              </p>
+                              <p style={{color: "white"}}>Student</p>
+                              <p>Lorem Ipsum is simply dummy text of the printing industry.</p>
+                            </div>
+                          </div>
+                        </div>
+                      }
+                    />
+                  </div>
                 </div>
               </div>
             </div>
